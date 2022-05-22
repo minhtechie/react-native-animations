37ded49cdc435279518f23666b2cf4585d205f57
minhtechie <juniorforlife>
2022-05-22 13:17:34 +0700

    feat: Clean up assets. Refactor ZingCarousel

--- src/data/musicData.ts
+++ src/data/musicData.ts
@@ -1,57 +1,32 @@
 export const musicData = [
   {
     id: '0',
-    image: require('../assets/images/zing/rap.png'),
-    bannerImage: require('../assets/images/zing/rapBanner.jpeg')
+    image: require('../assets/images/zing/children.png'),
+    bannerImage: require('../assets/images/zing/childrenBanner.jpeg'),
   },
   {
     id: '1',
-    image: require('../assets/images/zing/usuk.png'),
-    bannerImage: require('../assets/images/zing/usUk.jpeg')
+    image: require('../assets/images/zing/edm.png'),
+    bannerImage: require('../assets/images/zing/edmBanner.jpeg'),
   },
   {
     id: '2',
-    image: require('../assets/images/zing/aucotis.png'),
-    bannerImage: require('../assets/images/zing/aucotisBanner.jpeg')
+    image: require('../assets/images/zing/kpop.png'),
+    bannerImage: require('../assets/images/zing/kpopBanner.jpeg'),
   },
   {
     id: '3',
-    image: require('../assets/images/zing/kpop.png'),
-    bannerImage: require('../assets/images/zing/kPop.jpeg')
+    image: require('../assets/images/zing/oneAir.png'),
+    bannerImage: require('../assets/images/zing/oneAirBanner.jpeg'),
   },
   {
     id: '4',
-    image: require('../assets/images/zing/edm.png'),
-    bannerImage: require('../assets/images/zing/EDM.jpeg')
+    image: require('../assets/images/zing/usUk.png'),
+    bannerImage: require('../assets/images/zing/usUkBanner.jpeg'),
   },
   {
     id: '5',
-    image: require('../assets/images/zing/bolero.png'),
-    bannerImage: require('../assets/images/zing/bannerBolero.jpeg')
-  },
-  {
-    id: '6',
-    image: require('../assets/images/zing/oneair.png'),
-    bannerImage: require('../assets/images/zing/oneAir.jpeg')
-  },
-  {
-    id: '7',
-    image: require('../assets/images/zing/vpop.png'),
-    bannerImage: require('../assets/images/zing/vPop.jpeg'),
-  },
-  {
-    id: '8',
-    image: require('../assets/images/zing/xone.png'),
-    bannerImage: require('../assets/images/zing/xOne.jpeg'),
-  },
-  {
-    id: '9',
-    image: require('../assets/images/zing/indie.png'),
-    bannerImage: require('../assets/images/zing/indieBanner.jpeg'),
-  },
-  {
-    id: '10',
-    image: require('../assets/images/zing/children.png'),
-    bannerImage: require('../assets/images/zing/childrenBanner.jpeg'),
+    image: require('../assets/images/zing/xOne.png'),
+    bannerImage: require('../assets/images/zing/xOneBanner.jpeg'),
   },
 ];
--- src/vanilla-animation/ZingCarousel/ZingCarousel.tsx
+++ src/vanilla-animation/ZingCarousel/ZingCarousel.tsx
@@ -1,90 +1,47 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {
   View,
-  Text,
   StyleSheet,
   ScrollView,
   Image,
-  SafeAreaView,
-  TextInput,
   StatusBar,
   TouchableOpacity,
   FlatList,
+  Animated,
 } from 'react-native';
 import {musicData} from '../../data/musicData';
+import {WINDOW_HEIGHT} from '../../utils';
 
 const ZingCarousel = () => {
-  const [activeImageBanner, setActiveImageBanner] = useState<string>(
-    musicData[0].image,
-  );
+  const animatedValue = useRef(new Animated.Value(0)).current;
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle={'light-content'} />
-      <Image source={activeImageBanner} style={styles.bannerMusic} />
-      <SafeAreaView>
-        <View style={styles.boxHeader}>
-          <View style={styles.boxUser}>
-            <Image
-              source={require('../../assets/images/user.png')}
-              style={styles.userIcon}
-            />
-          </View>
-          <View style={styles.boxSearchInput}>
-            <View style={styles.boxSearchIconAndSearchInput}>
-              <Image
-                source={require('../../assets/images/momo/search.png')}
-                style={styles.searchIcon}
-              />
-              <TextInput
-                placeholder="Bài hát, playlist, nghệ sĩ..."
-                placeholderTextColor="rgba(255, 255, 255, 0.8)"
-                style={styles.searchInput}
-              />
-            </View>
-            <View style={styles.boxMircro}>
-              <Image
-                source={require('../../assets/images/zing/micro.png')}
-                style={styles.mircoIcon}
-              />
-            </View>
-          </View>
-          <View style={styles.boxBell}>
-            <Image
-              source={require('../../assets/images/momo/bell.png')}
-              style={styles.bellIcon}
-            />
-          </View>
-        </View>
-      </SafeAreaView>
+
+      <Image source={musicData[0].bannerImage} style={styles.banner} />
 
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.spaceTop} />
-        <View style={styles.boxContent}>
-          <FlatList
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            data={musicData}
-            renderItem={({item, index}) => {
-              return (
-                <TouchableOpacity
-                  style={styles.boxLiveMusic}
-                  onPress={() => {
-                    setActiveImageBanner(item.bannerImage);
-                  }}>
-                  <Image
-                    source={item.image}
-                    style={styles.imageLiveMusicItem}
-                  />
-                  <View style={styles.borderLiveMusicItem}>
-                    <Text style={styles.liveMusicItemLiveTitle}>LIVE</Text>
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-            keyExtractor={item => item.image}
-          />
-        </View>
+        <View style={styles.spaceForBanner} />
+        <FlatList
+          style={styles.carousel}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={musicData}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity style={styles.carouselItem} activeOpacity={1}>
+                <Image source={item.image} style={styles.carouselItemImage} />
+              </TouchableOpacity>
+            );
+          }}
+          keyExtractor={item => item.image}
+          onScroll={e => {
+            animatedValue.setValue(e.nativeEvent.contentOffset.x);
+          }}
+        />
+
+        <View style={styles.scrollViewContent} />
       </ScrollView>
     </View>
   );
@@ -95,101 +52,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  bannerMusic: {
+  banner: {
     width: '100%',
-    height: 400,
+    height: 320,
     position: 'absolute',
     resizeMode: 'cover',
   },
-  boxHeader: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 32,
-    justifyContent: 'space-around',
+  spaceForBanner: {
+    paddingTop: 320,
   },
-  boxUser: {flex: 1},
-  userIcon: {
-    width: 20,
-    height: 20,
-    tintColor: 'white',
+  scrollViewContent: {
+    height: WINDOW_HEIGHT * 2,
+    backgroundColor: '#09172a',
   },
-  boxSearchIconAndSearchInput: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  boxSearchInput: {
-    flex: 4,
-    flexDirection: 'row',
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
-    borderRadius: 60,
-    paddingVertical: 4,
-  },
-  searchIcon: {
-    width: 16,
-    height: 16,
-    marginLeft: 8,
-  },
-  searchInput: {
+  carousel: {
     position: 'absolute',
-    width: '100%',
-    color: 'white',
-    paddingVertical: 4,
-    paddingLeft: 32,
-    fontSize: 12,
+    top: 280,
+    zIndex: 100,
   },
-  boxMircro: {
-    backgroundColor: '#1e90ff',
-    borderRadius: 60,
-    width: 20,
-    height: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 4,
-    marginLeft: 4,
-  },
-  mircoIcon: {
-    width: 16,
-    height: 16,
-    tintColor: 'white',
-  },
-  boxBell: {flex: 1, alignItems: 'flex-end'},
-  bellIcon: {
-    width: 20,
-    height: 20,
-    tintColor: 'white',
-  },
-  boxContent: {
-    height: 600,
-    backgroundColor: 'white',
-  },
-  boxLiveMusic: {
+  carouselItem: {
     width: 80,
     height: 80,
-    marginRight: 4,
+    marginRight: 8,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  imageLiveMusicItem: {
+  carouselItemImage: {
     borderRadius: 60,
     width: 80,
     height: 80,
-    borderWidth: 2,
-    borderColor: 'red',
-    position: 'relative',
-  },
-  borderLiveMusicItem: {
-    position: 'absolute',
-    top: 72,
-    backgroundColor: 'red',
-  },
-  liveMusicItemLiveTitle: {
-    color: 'white',
-    fontSize: 8,
-    fontWeight: 'bold',
-    padding: 1,
-  },
-  spaceTop: {
-    paddingTop: 150,
   },
 });
 export default ZingCarousel;
